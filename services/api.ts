f877a5aaa0a03d053f39d1c151f459a6ff715949
vrushankReliable <vrushankreliablesolution@gmail.com>
2026-01-30 18:56:01 +0530

    vercel problem

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1/public",
+  baseURL: (
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1/public"
+  ).replace(/([^:]\/)\/+/g, "$1"),
   headers: {
     "Content-Type": "application/json",
     "x-api-key": process.env.NEXT_PUBLIC_API_KEY || "",
